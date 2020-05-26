e5227e491c99467d9c7723be71d97deb698eefba
Parikshith <pkedilay@asu.edu>
2020-05-25 19:13:13 -0700

    Rendered tweets

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,15 +1,15 @@
 import {getInitialData} from '../utils/api'
 import {receiveTweets} from './tweets'
 import {receiveUsers} from './users'
-import {setAuthedUser} from './authedUsers'
+import {setAuthedUser} from './authedUser'
 
-const AUTHED_ID = 'parikshithkm'
+const AUTHED_ID = 'tylermcginnis'
 export function handleInitialData() {
     return (dispatch) => {
         return getInitialData()
         .then(({users, tweets}) => {
-            dispatch(receiveTweets(tweets))
             dispatch(receiveUsers(users))
+            dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHED_ID))
         })
     }
